9b40cd0d0729562554400fb8144515e06bf7ee3b
neetnav <neetnavkayasth004@gmail.com>
2022-03-15 18:18:53 +0530

    Home page done

--- src/component/Messenger.jsx
+++ src/component/Messenger.jsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
+import { AppBar, Toolbar, makeStyles, Box } from "@material-ui/core";
 import Login from './account/Login';
 
 const useStyles = makeStyles({
+    component: {
+        background: '#DCDCDC',
+        height: '100vh'
+      
+    },
   loginHeader: {
     height: 200,
     background: "#00bfa5",
     boxShadow: "none",
-  },
+  }
+ 
 });
 
 const Messenger = () => {
   const classes = useStyles();
   return (
-      <>
+      <Box className= {classes.component}>
     <AppBar className={classes.loginHeader}>
       <Toolbar></Toolbar>
     </AppBar>
     <Login />
-    </>
+    </Box>
 
   );
 };
--- src/component/account/Login.jsx
+++ src/component/account/Login.jsx
@@ -9,7 +9,23 @@ const useStyles =makeStyles({
     qrCode:{
         height:264,
         width:264,
-        padding: '50px 0 0 50px'
+        padding: '50px 0 0 200px'
+    },
+    title:{
+        fontSize:26,
+        marginBottom:25,
+        color:'#525252',
+        fontWeight:300
+    },
+    list:{
+
+        '& >*':{
+            fontSize:18,
+            padding:0,
+            marginTop:15,
+            lineHeight: '28px',
+            color: '#4a4a4a'
+        }
     }
 
 })
@@ -35,8 +51,8 @@ const Login = ({classes}) => {
              BackdropProps={{style:{backgroundColor:'unset'}}}>
             <Box className={classname.component}>
                 <Box className={classname.leftcomponent}>
-                    <Typography>To use wp on ur pc</Typography>
-                    <List>
+                    <Typography className={classname.title}>To use wp on ur pc</Typography>
+                    <List className={classname.list}>
                         <ListItem>1. open wp on ur phone</ListItem>
                         <ListItem>2. tap menu</ListItem>
                         <ListItem>3. point phone on screen</ListItem>
@@ -44,7 +60,7 @@ const Login = ({classes}) => {
 
                 </Box>
                 <Box>
-                    <img src={qrurl} alt='qr'  className="classname.qrCode"/>
+                    <img src={qrurl} alt='qr'  className={classname.qrCode}/>
                 </Box>
             </Box>
         
